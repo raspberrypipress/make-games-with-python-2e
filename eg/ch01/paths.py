95e0df8a942c1adf45871e93c2be5bc748d1522f
Brian Jepson <brian.jepson@raspberrypi.com>
2025-02-06 16:43:32 -0500

    ch01 examples

---
+++ eg/ch01/paths.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((500, 400))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    # Begin drawing statements
+    pygame.draw.line(window, (255,255,255),
+                     (0, 0), (500, 400), 1)
+    # End drawing statements
+
+    pygame.display.flip()
+    clock.tick(60)
