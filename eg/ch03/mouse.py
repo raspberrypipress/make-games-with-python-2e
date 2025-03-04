45255ae54f3340f4e5c380cc4011bd16548048d8
Brian Jepson <bjepson@gmail.com>
2025-03-04 13:36:20 +0000

    tightened up code

--- eg/ch03/mouse.py
+++ eg/ch03/mouse.py
@@ -14,40 +14,40 @@ pygame.display.set_caption('Pygame Mouse!')
 square_size = 40
 square_x = win_width / 2
 square_y = win_height - square_size
-gravity = 5.0
+gravity = 2.0
+mouse_pressed = False
+is_dragging = False
 
-def checkBounds(mouse_pressed, is_dragging):
+def checkBounds():
+
+    global is_dragging
 
     if mouse_pressed:
         box = pygame.Rect((square_x, square_y, square_size, square_size))
         if box.collidepoint(pygame.mouse.get_pos()):
             is_dragging = True
             pygame.mouse.set_visible(0)
-
     else:
         pygame.mouse.set_visible(1)
         is_dragging = False
 
-    return is_dragging
-
-def checkGravity(mouse_pressed):
+def checkGravity():
 
     global gravity, square_y
 
     # Is our square in the air and have we let go of it?
     if square_y < win_height - square_size and not mouse_pressed:
         square_y += gravity
-        gravity = gravity * 1.1
+        gravity = gravity * 1.05
     else:
         square_y = win_height - square_size
-        gravity = 5.0
+        gravity = 2.0
 
-def drawSquare(is_dragging):
+def drawSquare():
 
     global square_x, square_y
 
-    if is_dragging == True:
-
+    if is_dragging:
         square_colour = (0, 255, 0)
         mouse_pos = pygame.mouse.get_pos()
         square_x = mouse_pos[0] - square_size / 2
@@ -57,15 +57,13 @@ def drawSquare(is_dragging):
 
     pygame.draw.rect(surface, square_colour, (square_x, square_y, square_size, square_size))
 
-mouse_pressed = False
-is_dragging = False
 while True:
 
     surface.fill((0,0,0))
 
-    is_dragging = checkBounds(mouse_pressed, is_dragging)
-    checkGravity(mouse_pressed)
-    drawSquare(is_dragging)
+    checkBounds()
+    checkGravity()
+    drawSquare()
 
     pygame.display.update()
     clock.tick(60)
