d923f4038e5a2d48e9e3cd9b8a23158fed9d6837
Brian Jepson <brian.jepson@raspberrypi.com>
2025-03-14 16:05:20 -0400

    latest updates from the manuscript

--- eg/ch03/mouse.py
+++ eg/ch03/mouse.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame
 
 # Pygame setup and variables
 pygame.init()
@@ -15,15 +15,16 @@ square_size = 40
 square_x = win_width / 2
 square_y = win_height - square_size
 gravity = 2.0
-mouse_pressed = False
+pressed = False
 is_dragging = False
 
 def checkBounds():
 
     global is_dragging
 
-    if mouse_pressed:
-        box = pygame.Rect((square_x, square_y, square_size, square_size))
+    if pressed:
+        box = pygame.Rect((square_x, square_y, 
+                           square_size, square_size))
         if box.collidepoint(pygame.mouse.get_pos()):
             is_dragging = True
             pygame.mouse.set_visible(0)
@@ -35,10 +36,11 @@ def checkGravity():
 
     global gravity, square_y
 
-    # Is our square in the air and have we let go of it?
-    if square_y < win_height - square_size and not mouse_pressed:
-        square_y += gravity
-        gravity = gravity * 1.05
+    # Is our square in the air 
+    if square_y < win_height - square_size:
+        if not is_dragging: # have we let go of it?
+            square_y += gravity
+            gravity = gravity * 1.05
     else:
         square_y = win_height - square_size
         gravity = 2.0
@@ -55,11 +57,19 @@ def drawSquare():
     else:
         square_colour = (255,0,0)
 
-    pygame.draw.rect(surface, square_colour, (square_x, square_y, square_size, square_size))
+    pygame.draw.rect(surface, square_colour,
+                     (square_x, square_y,
+                      square_size, square_size))
 
 while True:
 
     surface.fill((0,0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+    pressed = pygame.mouse.get_pressed()[0]
 
     checkBounds()
     checkGravity()
@@ -67,9 +77,3 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
-
-    for event in pygame.event.get():
-        mouse_pressed = pygame.mouse.get_pressed()[0]
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            raise SystemExit
