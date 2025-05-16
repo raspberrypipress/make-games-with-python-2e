62afdb34cc8c4da08e613654a1a5343647845a92
Brian Jepson <bjepson@gmail.com>
2025-05-16 16:41:46 +0100

    refactoring, tabs to spaces, etc.

--- eg/ch07/collisions.py
+++ eg/ch07/collisions.py
@@ -1,203 +1,208 @@
 import pygame, sys, math, random
-import pygame.locals as GAME_GLOBALS
-import pygame.event as GAME_EVENTS
-import pygame.time as GAME_TIME
 from pygame.math import Vector2
-
-windowWidth = 1024
-windowHeight = 768
 
 pygame.init()
 clock = pygame.time.Clock()
-surface = pygame.display.set_mode((windowWidth, windowHeight))
+fps = 60
 
+win_width = 1024
+win_height = 768
+window = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption('Collisions')
 
-previousMousePosition = [0,0]
-mousePosition = None
-mouseDown = False
+prev_mouse_pos = Vector2()
+mouse_pos = None
+mouse_down = False
 
 collidables = []
-currentObject = None
+current_obj = None
 expanding = True
 
-drawAttractions = False
+draw_attractions = False
 
 gravity = 1.0
 
-def drawCollidables():
+def draw_collidables():
 
-	for obj in collidables:
-		obj["pos"] += obj["velocity"]
+    for obj in collidables:
+        obj["pos"] += obj["velocity"]
+        pygame.draw.circle(window, obj["colour"], 
+                           obj["pos"], int(obj["radius"]), 0)
 
-		pygame.draw.circle(surface, obj["colour"], obj["pos"], int(obj["radius"]), 0)
+def draw_current_object():
 
-def drawCurrentObject():
+    global expanding, current_obj
 
-	global expanding, currentObject
+    current_obj["pos"] = mouse_pos
 
-	currentObject["pos"] = Vector2(mousePosition)
+    if expanding is True and current_obj["radius"] < 60:
+        current_obj["radius"] += 0.2
 
-	if expanding is True and currentObject["radius"] < 60:
-		currentObject["radius"] += 0.2
+        if current_obj["radius"] >= 60:
+            expanding = False
+            current_obj["radius"] = 9.9
 
-		if currentObject["radius"] >= 60:
-			expanding = False
-			currentObject["radius"] = 9.9
+    elif expanding is False and current_obj["radius"] > 1:
+        current_obj["radius"] -= 0.2
 
-	elif expanding is False and currentObject["radius"] > 1:
-		currentObject["radius"] -= 0.2
+        if current_obj["radius"] <= 1:
+            expanding = True
+            current_obj["radius"] = 1.1
 
-		if currentObject["radius"] <= 1:
-			expanding = True
-			currentObject["radius"] = 1.1
+    current_obj["mass"] = current_obj["radius"]
 
-	currentObject["mass"] = currentObject["radius"]
+    pygame.draw.circle(window, (255,0,0), 
+                       current_obj["pos"], 
+                       int(current_obj["radius"]), 0)
 
-	pygame.draw.circle(surface, (255,0,0), currentObject["pos"], int(currentObject["radius"]), 0)
+def calculate_movement():
 
-def calculateMovement():
+    for o in collidables:
 
-	for obj in collidables:
+        other_objs = [x for x in collidables if x is not o]
+        for other in other_objs:
+                
+            direction = other["pos"] - o["pos"]
+            magnitude = other["pos"].distance_to(o["pos"])
+            n_direction = direction / magnitude
 
-		other_objs = [x for x in collidables if x is not obj]
-		for other in other_objs:
-				
-			direction = other["pos"] - obj["pos"]
-			magnitude = other["pos"].distance_to(obj["pos"])
-			nDirection = direction / magnitude
+            if magnitude < 5:
+                magnitude = 5
+            elif magnitude > 15:
+                magnitude = 15
 
-			if magnitude < 5:
-				magnitude = 5
-			elif magnitude > 15:
-				magnitude = 15
+            strength = ((gravity * o["mass"] * other["mass"]) /
+                        (magnitude * magnitude)) / other["mass"]
 
-			strength = ((gravity * obj["mass"] * other["mass"]) / (magnitude * magnitude)) / other["mass"]
-			appliedForce = nDirection * strength
-			other["velocity"] -= Vector2(appliedForce)
+            applied_force = n_direction * strength
+            other["velocity"] -= Vector2(applied_force)
 
-			if drawAttractions is True:
-				pygame.draw.line(surface, (255,255,255), obj["pos"], other["pos"], 1)
+            if draw_attractions is True:
+                pygame.draw.line(window, (255,255,255), 
+                                 o["pos"], other["pos"], 1)
 
-def handleCollisions():
+def handle_collisions():
 
-	for obj in collidables:
+    for o in collidables:
 
-		other_objs = [x for x in collidables if x is not obj]
-		for other in other_objs:
+        other_objs = [x for x in collidables if x is not o]
+        for other in other_objs:
 
-			distance = other["pos"].distance_to(obj["pos"])
-			if distance < other["radius"] + obj["radius"]:
-				print(other["radius"] + obj["radius"] - distance)
+            distance = other["pos"].distance_to(o["pos"])
+            if distance < other["radius"] + o["radius"]:
+                print(other["radius"]+o["radius"]-distance)
 
-				# Angle of the collision between the two
-				coll_angle = math.atan2( 
-					*reversed(obj["pos"] - other["pos"]))
+                # Angle of the collision between the two
+                coll_angle = math.atan2( 
+                    *reversed(o["pos"] - other["pos"]))
 
-				# Calculate the speed of each object
-				obj_speed = obj["velocity"].magnitude()
-				other_speed = other["velocity"].magnitude()
+                # Calculate the speed of each object
+                obj_speed = o["velocity"].magnitude()
+                other_speed = other["velocity"].magnitude()
 
-				# Get direction of the objects in radians
-				obj_dir = math.atan2(
-					*reversed(obj["velocity"]))
-				other_dir = math.atan2(
-					*reversed(other["velocity"]))
+                # Get direction of the objects in radians
+                obj_dir = math.atan2(
+                    *reversed(o["velocity"]))
+                other_dir = math.atan2(
+                    *reversed(other["velocity"]))
 
-				# Calculate the post-collision velocity
-				obj_angle = obj_dir - coll_angle
-				obj_new_ang = Vector2(math.cos(obj_angle),
-				                      math.sin(obj_angle))
-				obj_new_vel = obj_new_ang * obj_speed
+                # Calculate the post-collision velocity
+                obj_angle = obj_dir - coll_angle
+                obj_new_ang = Vector2(math.cos(obj_angle),
+                                      math.sin(obj_angle))
+                obj_new_vel = obj_new_ang * obj_speed
 
-				other_angle = other_dir - coll_angle
-				other_new_ang = Vector2(math.cos(other_angle),
-								        math.sin(other_angle))
-				other_new_vel = other_new_ang * other_speed
-				
-				# Adjust velocity based on object masses
-				mass = obj["mass"]
-				other_mass = other["mass"]
+                other_angle = other_dir - coll_angle
+                other_new_ang = Vector2(math.cos(other_angle),
+                                        math.sin(other_angle))
+                other_new_vel = other_new_ang * other_speed
+                
+                # Adjust velocity based on object masses
+                mass = o["mass"]
+                other_mass = other["mass"]
 
-				obj_final_vel = (
-					((mass - other_mass) * obj_new_vel
-					 + (other_mass * 2) * other_new_vel)
-					/ (mass + other_mass)
-				)
-				other_final_vel = (
-					((mass * 2) * obj_new_vel
-					 + (other_mass - mass) * other_new_vel)
-					/ (mass + other_mass)
-				)
+                obj_final_vel = (
+                    ((mass - other_mass) * obj_new_vel
+                     + (other_mass * 2) * other_new_vel)
+                    / (mass + other_mass)
+                )
+                other_final_vel = (
+                    ((mass * 2) * obj_new_vel
+                     + (other_mass - mass) * other_new_vel)
+                    / (mass + other_mass)
+                )
 
-				# Set the final velocities
-				obj["velocity"] = obj_final_vel
-				other["velocity"] = other_final_vel
+                # Set the final velocities
+                o["velocity"] = obj_final_vel
+                other["velocity"] = other_final_vel
 
-def handleMouseDown():
-	global currentObject
+def handle_mouse_down():
+    global current_obj
 
-	currentObject = {
-		"radius" : 3,
-		"mass" : 3,
-		"velocity" : Vector2(),
-		"pos" : Vector2(),
-		"colour" : random.choices(range(256), k=3)
-	}
+    current_obj = {
+        "radius" : 3,
+        "mass" : 3,
+        "velocity" : Vector2(),
+        "pos" : Vector2(),
+        "colour" : random.choices(range(256), k=3)
+    }
 
 def quitGame():
-	pygame.quit()
-	sys.exit()
+    pygame.quit()
+    sys.exit()
 
 # 'main' loop
 while True:
 
-	surface.fill((0,0,0))
-	mousePosition = pygame.mouse.get_pos()
+    window.fill((0,0,0))
+    mouse_pos = Vector2(pygame.mouse.get_pos())
 
-	# Handle user and system events 
-	for event in GAME_EVENTS.get():
+    # Handle user and system events 
+    for event in pygame.event.get():
 
-		if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
 
-			if event.key == pygame.K_ESCAPE:
-				quitGame()
+            if event.key == pygame.K_ESCAPE:
+                quitGame()
 
-		if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP:
 
-			if event.key == pygame.K_r:
-				collidables = []
-			if event.key == pygame.K_a:
-				if drawAttractions is True:
-					drawAttractions = False
-				elif drawAttractions is False:
-					drawAttractions = True
+            if event.key == pygame.K_r:
+                collidables = []
+            if event.key == pygame.K_a:
+                if draw_attractions is True:
+                    draw_attractions = False
+                elif draw_attractions is False:
+                    draw_attractions = True
 
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			mouseDown = True
-			handleMouseDown()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_down = True
+            handle_mouse_down()
 
-		if event.type == pygame.MOUSEBUTTONUP:
-			mouseDown = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_down = False
 
-		if event.type == GAME_GLOBALS.QUIT:
-			quitGame()
+        if event.type == pygame.QUIT:
+            quitGame()
 
-	calculateMovement()
-	handleCollisions()
-	drawCollidables()
+    calculate_movement()
+    handle_collisions()
+    draw_collidables()
 
-	if currentObject is not None:
-		drawCurrentObject()
+    if current_obj is not None:
+        draw_current_object()
 
-		# If our user has released the mouse, add the new obj to the collidables list and let gravity do its thing
-		if mouseDown is False:
-			currentObject["velocity"] = (Vector2(mousePosition) - Vector2(previousMousePosition)) / 4
-			collidables.append(currentObject)
-			currentObject = None
+        # If our user has released the mouse, add the new obj
+        # to the collidables list and let gravity do its thing
+        if mouse_down is False:
+            v = (mouse_pos - prev_mouse_pos) / 4
+            current_obj["velocity"] = v 
+            collidables.append(current_obj)
+            current_obj = None
 
-	# Store the previous mouse coordinates to create a vector when we release a new obj
-	previousMousePosition = mousePosition
+    # Store the previous mouse coordinates to create a vector
+    # when we release a new obj
+    prev_mouse_pos = mouse_pos
 
-	clock.tick(60)
-	pygame.display.update()
+    clock.tick(60)
+    pygame.display.update()
