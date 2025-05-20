5ba078f84694fde3a96295da8ee4df7182f80dbd
Brian Jepson <bjepson@gmail.com>
2025-05-20 15:51:40 +0100

    a little rearrangement

--- code/ch05/sounds.py
+++ code/ch05/sounds.py
@@ -13,6 +13,9 @@ pygame.display.set_caption("Soundboard")
 
 imgs = "assets/images"
 snds = "assets/sounds"
+buttons = []
+stop_btn = {"image" : image.load(f"{imgs}/stop.png"),
+            "pos" : (275, 585)}
 
 volume = 0.2
 volume_slider_rect = pygame.Rect(450, 610, 100, 5)
@@ -21,16 +24,22 @@ flashed = None
 CLEAR_FLASH = pygame.USEREVENT + 0
 flash_timer = 250
 
+# Initialise the mixer and play a sound
 pygame.mixer.init()
 pygame.mixer.music.load(f"{snds}/OGG/farm.ogg")
 pygame.mixer.music.play(-1)
 
-def flash_button(img):    
-    inv = pygame.Surface(img.get_rect().size, pygame.SRCALPHA)
-    inv.blit(img, (0,0), None)
-    inv.fill((255, 255, 255, 128), None, 
-             pygame.BLEND_RGBA_MULT)
-    return inv
+# Create buttons
+animals = ["sheep", "rooster", "pig", "mouse",
+           "horse", "dog", "cow", "chicken", "cat"]
+for y in range(0, 3):
+    for x in range(0, 3):
+        animal = animals.pop(0)
+        img = image.load(f"{imgs}/{animal}.png")
+        snd = mixer.Sound(f"{snds}/OGG/{animal}.ogg")
+        buttons.append({"image": img,
+                        "pos": (x * 200 + 25, y * 200 + 25),
+                        "sound": snd})
 
 def draw_buttons():
     for button in buttons + [stop_btn]:
@@ -47,6 +56,14 @@ def draw_volume():
 
     pygame.draw.rect(window, (204, 204, 204), 
                      (450 + volume_pos, 600, 10, 25))
+
+def checkVolume():
+    global volume
+
+    if pygame.mouse.get_pressed()[0]:
+        mouse_pos = pygame.mouse.get_pos()
+        if volume_slider_rect.collidepoint(pygame.mouse.get_pos()):
+            volume = float((mouse_pos[0] - 450)) / 100
 
 def handle_click():
     global flashed
@@ -66,33 +83,18 @@ def handle_click():
         pygame.time.set_timer(CLEAR_FLASH, flash_timer)
         mixer.stop()
 
-def checkVolume():
-    global volume
-
-    if pygame.mouse.get_pressed()[0]:
-        mouse_pos = pygame.mouse.get_pos()
-        if volume_slider_rect.collidepoint(pygame.mouse.get_pos()):
-            volume = float((mouse_pos[0] - 450)) / 100
+def flash_button(img):    
+    inv = pygame.Surface(img.get_rect().size, pygame.SRCALPHA)
+    inv.blit(img, (0,0), None)
+    inv.fill((255, 255, 255, 128), None, 
+             pygame.BLEND_RGBA_MULT)
+    return inv
 
 def quitGame():
     pygame.quit()
     raise SystemExit
 
 
-# Create Buttons
-buttons = []
-stop_btn = {"image" : image.load(f"{imgs}/stop.png"),
-               "pos" : (275, 585)}
-animals = ["sheep", "rooster", "pig", "mouse", "horse", 
-           "dog", "cow", "chicken", "cat"]
-for y in [25, 225, 425]:
-    for x in [25, 225, 425]:
-        animal = animals.pop(0)
-        img = image.load(f"{imgs}/{animal}.png")
-        snd = mixer.Sound(f"{snds}/OGG/{animal}.ogg")
-        buttons.append({"image": img,
-                        "pos": (x, y),
-                        "sound": snd})
 
 # main loop
 while True:
