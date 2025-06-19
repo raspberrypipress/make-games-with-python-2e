ce3bbf5c43bb000177306af5ea69eb5948c35d06
Brian Jepson <brian.jepson@raspberrypi.com>
2025-06-19 08:38:37 -0400

    added comments to make the swap more obvious

--- code/ch01/overlap_3_rects_redux.py
+++ code/ch01/overlap_3_rects_redux.py
@@ -9,9 +9,9 @@ while True:
             raise SystemExit
 
     # Begin drawing statements
-    pygame.draw.rect(window, (255,0,0), (0, 0, 50, 50))
-    pygame.draw.rect(window, (0,0,255), (80, 0, 50, 50))
-    pygame.draw.rect(window, (0,255,0), (40, 0, 50, 50))
+    pygame.draw.rect(window, (255,0,0), (0, 0, 50, 50))  # 1
+    pygame.draw.rect(window, (0,0,255), (80, 0, 50, 50)) # 3
+    pygame.draw.rect(window, (0,255,0), (40, 0, 50, 50)) # 2
     # End drawing statements
 
     pygame.display.update()
